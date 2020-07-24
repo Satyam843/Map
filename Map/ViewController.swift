71f06a5fc4b03f013fd99c71c36d579108115921
satyam <satyam.kumar@jungleworks.com>
2020-07-24 12:56:21 +0530

    implemented map view

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userMapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    fileprivate let  locationManager :  CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.requestWhenInUseAuthorization()
+        return manager
+    }()
+    func currentLocation()
+    {
+        locationManager.delegate = self as! CLLocationManagerDelegate
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        if #available(iOS 12.0, *)
+        {
+            locationManager.showsBackgroundLocationIndicator = true
+        }
+        else
+        {
+            
+        }
+       locationManager.startUpdatingLocation()
+    }
 
 
+}
+extension ViewController : CLLocationManagerDelegate
+{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        let currentLocation =  location.coordinate
+        let coordinateRegion = MKCoordinateRegion(center:currentLocation, latitudinalMeters: 800, longitudinalMeters: 800)
+        userMapView.setRegion(coordinateRegion, animated: true)
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
 }
 
